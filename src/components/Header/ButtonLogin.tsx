010ca3af4c18ee2d13dbf6cc85a914d152adcd11
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-01-29 16:43:01 -0300

    feat(sessionButtons): Create basic structure of ButtonLogin and ButtonRegister component

---
+++ src/components/Header/ButtonLogin.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+export const ButtonLogin:React.FC = () => {
+  return (
+    <button className='button-login'>Log in</button>
+  )
+}
---
+++ src/components/Header/ButtonRegister.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ButtonRegister:React.FC  = () => {
+  return (
+    <button className='button-register'>Register</button>
+  )
+}
