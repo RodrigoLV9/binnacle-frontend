3cd4aa7c1048586b242f695ae8ae7605fa50516f
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-01-29 18:07:10 -0300

    feat(Hero): Create basic structure for Hero component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../styles/Hero.css'
+export const Hero:React.FC = () => {
+  return (
+    <section className='hero'>
+        <h1>Binnacle App</h1>
+        <p>For productivity</p>
+    </section>
+  )
+}
