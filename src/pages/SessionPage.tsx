40f433c853728ca05d5e4d2f7d0377531fc9ed4e
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-02-01 00:11:58 -0300

    feat(SessionPage): Add content for SessionPage component

--- src/pages/SessionPage.tsx
+++ src/pages/SessionPage.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { ButtonMode } from '../components/Header/ButtonMode'
+import '../styles/SessionPage.css'
+import { FormSession } from '../components/FormSession'
 export const SessionPage:React.FC = () => {
   return (
     <section>
-        Session page
+        <header className='header-session'>
+          <img src="./images/binnacle-logo.png" alt="binnacle main logo" width={50} height={50} />
+          <ButtonMode/>
+        </header>
+        <FormSession/>
     </section>
   )
 }
